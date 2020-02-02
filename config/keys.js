177f541e6512a8df2eb482a8664205188c55ce66
stevehind <steve.hind@gmail.com>
2020-02-01 19:16:08 -0800

    added .gitignore

--- config/keys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    mongoURI_example: "mongodb://admin:JF5ZnmAr8CDCnHcG@ds159993.mlab.com:59993/mern-auth", 
-    mongoURI: "mongodb+srv://admin:JF5ZnmAr8CDCnHcG@mern-auth-syhqh.mongodb.net/test?retryWrites=true&w=majority",
-    secretOrKey: "secret"
-  };
